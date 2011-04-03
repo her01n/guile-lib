5ceb6e6c5b46313419830f3754c4c68666121c2c
Andy Wingo <wingo@pobox.com>
2011-04-03 19:44:20 +0200

    www news for 0.2.1
    * doc/www/news/index.scm: Add release note.

--- doc/www/news/index.scm
+++ doc/www/news/index.scm
@@ -2,6 +2,13 @@
   '((h2 "news")
 
     (news
+     (@ (date "3 April 2011")
+        (title "guile-lib 0.2.1 available"))
+     (code "guile-lib") " 0.2.1 has been released. Check the "
+     (a (@ (href "http://download.savannah.gnu.org/releases/guile-lib/NEWS")) "NEWS")
+     " for details.")
+    
+    (news
      (@ (date "26 March 2011")
         (title "guile-lib 0.2.0 available"))
      (code "guile-lib") " 0.2.0 has been released. Check the "
