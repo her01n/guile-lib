d6f82af4e61f17ac2c4e6291e31d64cf86dc834d
David Pirotte <david@altosw.be>
2016-07-12 21:21:07 -0300

    Updating autogen and configure
    * autogen.sh: (a) removing the call to configure, printing an invitation
      to do so instead; (b) creating build-aux/config.rpath if it does not
      exists: this because we use configure.ac uses the guile.m4 GUILE_FLAGS
      macro, among others, which needs this file, not installed anymore by
      modern version of automake, iiuc, and without it, autogen raises an
      an error. For why it's needed, see the comments in m4/guile.m4.
    
    * configure.ac: Adding a copyright; bumping prereq -> 2.69, version ->
      0.2.3-dev; let's use build-aux, m4; automake -> 1.14, serial-tests;
      checks/looks for guile 2.2, 2.0; cleaning HAVE_GUILE_COMPILE checks,
      we always have access to a compiler: in the end this wil disappear,
      but we kept and set HAVE_GUILE_COMPILE=1 still used 'en aval'; calling
      GUILE_GLOBAL_SITE_DIR and GUILE_SITE_CCACHE_DIR, we'll use them in
      Makefile.am.

--- autogen.sh
+++ autogen.sh
@@ -7,5 +7,16 @@ set -e
 echo '@setfilename guile-library.info' > doc/guile-library.texi
 touch -d 'jan 23 1980' doc/guile-library.texi
 
+# configure.ac uses the guile.m4 GUILE_FLAGS macro, among others,
+# which needs build-aux/config.rpath which is not installed anymore by
+# modern version of automake, and without it, this script will raise
+# an error. For why it's needed, see the comments wtr in m4/guile.m4.
+if [ ! -d "build-aux" ]; then
+    mkdir build-aux
+fi
+touch build-aux/config.rpath
+
 autoreconf -vif
-exec ./configure "$@"
+
+echo
+echo "Now run ./configure --prefix=/your/prefix."
