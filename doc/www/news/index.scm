3e0c3504b36cb91d5ceb147542b4602985a3324f
David Pirotte <david@altosw.be>
2016-11-13 23:05:32 -0200

    Updating NEWS and web-pages for the next release
    * NEWS:
    * doc/www/news/index.scm:  Updated for the next release.

--- doc/www/news/index.scm
+++ doc/www/news/index.scm
@@ -2,6 +2,13 @@
   '((h2 "news")
 
     (news
+     (@ (date "14 November 2016")
+        (title "guile-lib 0.2.4 available"))
+     (code "guile-lib") " 0.2.4 has been released. Check the "
+     (a (@ (href "http://download.savannah.gnu.org/releases/guile-lib/NEWS")) "NEWS")
+     " for details.")
+
+    (news
      (@ (date "19 September 2016")
         (title "guile-lib 0.2.3 available"))
      (code "guile-lib") " 0.2.3 has been released. Check the "
